e218f1766b1a900cfb9d7cc2816bb826be81889b
rulasg <rulasg@outlook.com>
2023-10-10 16:43:12 +0200

    adding testing

--- public/gettrainingusers.ps1
+++ public/gettrainingusers.ps1
@@ -1,5 +1,5 @@
 function Get-ClassUsers{
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess)]
     param(
         # Target repo
         [Parameter(Mandatory,Position=0, ValueFromPipeline)]
@@ -10,20 +10,30 @@ function Get-ClassUsers{
 
         $issueId = Find-FirstActivityIssue -ClassRepo $ClassRepo
         
-        $command = 'gh issue view {issueId}  -R ps-developers-sandbox/{repo} --json body | convertfrom-json'
+        $command = 'gh issue view {issueId}  -R ps-developers-sandbox/{repo} --json body'
         $command = $command -replace '{issueId}', $issueId
         $command = $command -replace '{repo}', $ClassRepo
         
-        $result = Invoke-Expression $command
+        if ($PSCmdlet.ShouldProcess("Invoke-GhExpression", $command)) {
+            $result = Invoke-GhExpression $command
+        } else {
+            Write-Information $command
+            # Mock asnwer for testing
+            $result = $null
+        }
         
-        $list = $result.body -split "`n" | Where-Object{$_[0] -eq '-'} | %{ ($_ -split '@')[1]}
+        if(!$result){
+            return $null
+        }
+
+        $list = $result.body -split "`n" | Where-Object{$_[0] -eq '-'} | ForEach-Object{ ($_ -split '@')[1]}
         
         return $list
     }
 } Export-ModuleMember -Function Get-ClassUsers
 
 function Find-FirstActivityIssue{
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess)]
     param(
         # Target repo
         [Parameter(Mandatory, Position=0)]
@@ -32,12 +42,20 @@ function Find-FirstActivityIssue{
 
     $ISSUE_TITLE = 'Activity 1: Your First Caption'
 
-    $command = 'gh issue list -R ps-developers-sandbox/{repo} --json title,number -s all| convertfrom-json'
+    $command = 'gh issue list -R ps-developers-sandbox/{repo} --json title,number -s all'
 
     $command = $command -replace '{repo}', $ClassRepo
 
-    $result = Invoke-Expression $command
-
+    if ($PSCmdlet.ShouldProcess("Invoke-GhExpression", $command)) {
+        $result = Invoke-GhExpression $command
+    } else {
+        Write-Information $command
+        # mock answer for testing
+        $result = [PSCustomObject]@{
+            title = $ISSUE_TITLE
+            number = 1
+        }
+    }
     $issue = $result | Where-Object{$_.title -match $ISSUE_TITLE}
 
     #check if no issue found
