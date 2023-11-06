10f41a2279b23ad982377718a61345ec9c0c6b38
rulasg <rulasg@outlook.com>
2023-11-06 23:58:22 +0100

    refactor Get-ClassRepo

--- public/getClassRepo.ps1
+++ public/getClassRepo.ps1
@@ -2,35 +2,25 @@ function Get-ClassRepo{
     [CmdletBinding(SupportsShouldProcess)]
     param(
         [Parameter(Mandatory)][string]$Name,
-        [Parameter()][string]$Topic,
         [Parameter()][string]$Owner
    )
 
    process {
 
         $limit = 1000
-        $attributes = "nameWithOwner,name"
+        $attributes = "fullName,name"
 
         $owner = Resolve-Owner -Owner $Owner
 
-        $command = 'gh repo list {owner} --limit {limt} --json {attributes}'
+        $command = 'gh search repos {reponame} in:name --owner {owner} --json {attributes}'
+        $command = $command -replace "{reponame}",$Name
         $command = $command -replace "{owner}",$owner
         $command = $command -replace "{limt}",$limit
         $command = $command -replace "{attributes}",$attributes
 
-        # check if $topic is not null or whitespaces
-        if(![string]::IsNullOrWhiteSpace($Topic)){
-            $command = $command + " --topic $Topic"
-        }
-
         $result = Invoke-GhExpression $command
-        
-        #check if $result is null
-        if(!$result){
-            return $null
-        }
 
-        $ret = $result | Where-Object {$_.name -like $Name}
+        $ret = $result
 
         return  $ret
 
